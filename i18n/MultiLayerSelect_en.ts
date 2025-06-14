aafd0caf42f573fb9d8d066ca94e4baea5568545
Yoann Quenach de Quivillic <yquenach@itlink.fr>
2025-06-15 00:41:42 +0200

    Fix translation

--- i18n/MultiLayerSelect_en.ts
+++ i18n/MultiLayerSelect_en.ts
@@ -1,75 +1,75 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en">
+<TS version="2.1" language="en_US">
 <context>
     <name>MultiLayerSelect</name>
     <message>
-        <location filename="../multilayerselect.py" line="147"/>
+        <location filename="../multilayerselect.py" line="148"/>
         <source>Select Features</source>
         <translation>Select Features</translation>
     </message>
     <message>
-        <location filename="../multilayerselect.py" line="147"/>
+        <location filename="../multilayerselect.py" line="148"/>
         <source>&lt;b&gt;Select Features by area or single click&lt;/b&gt;</source>
         <translation>&lt;b&gt;Select Features by area or single click&lt;/b&gt;</translation>
     </message>
     <message>
-        <location filename="../multilayerselect.py" line="154"/>
+        <location filename="../multilayerselect.py" line="155"/>
         <source>Select Features by Polygon</source>
         <translation>Select Features by Polygon</translation>
     </message>
     <message>
-        <location filename="../multilayerselect.py" line="160"/>
+        <location filename="../multilayerselect.py" line="161"/>
         <source>Select Features by Freehand</source>
         <translation>Select Features by Freehand</translation>
     </message>
     <message>
-        <location filename="../multilayerselect.py" line="166"/>
+        <location filename="../multilayerselect.py" line="167"/>
         <source>Select Features by Radius</source>
         <translation>Select Features by Radius</translation>
     </message>
     <message>
-        <location filename="../multilayerselect.py" line="296"/>
+        <location filename="../multilayerselect.py" line="297"/>
         <source>About Multilayer Select</source>
         <translation>About Multilayer Select</translation>
     </message>
     <message>
-        <location filename="../multilayerselect.py" line="296"/>
+        <location filename="../multilayerselect.py" line="297"/>
         <source>Source code</source>
         <translation>Source code</translation>
     </message>
     <message>
-        <location filename="../multilayerselect.py" line="296"/>
+        <location filename="../multilayerselect.py" line="297"/>
         <source>Report issues</source>
         <translation>Report issues</translation>
     </message>
     <message>
-        <location filename="../multilayerselect.py" line="296"/>
+        <location filename="../multilayerselect.py" line="297"/>
         <source>Documentation</source>
         <translation>Documentation</translation>
     </message>
     <message>
-        <location filename="../multilayerselect.py" line="218"/>
+        <location filename="../multilayerselect.py" line="219"/>
         <source>Select Features by Expression...</source>
         <translation>Select Features by Expression...</translation>
     </message>
     <message>
-        <location filename="../multilayerselect.py" line="118"/>
+        <location filename="../multilayerselect.py" line="119"/>
         <source>Multilayer Select Settings</source>
         <translation>Multilayer Select Settings</translation>
     </message>
     <message>
-        <location filename="../multilayerselect.py" line="205"/>
+        <location filename="../multilayerselect.py" line="206"/>
         <source>Select All Features from All Layers</source>
         <translation>Select All Features from All Layers</translation>
     </message>
     <message>
-        <location filename="../multilayerselect.py" line="212"/>
+        <location filename="../multilayerselect.py" line="213"/>
         <source>Invert Selection for All Layers</source>
         <translation>Invert Selection for All Layers</translation>
     </message>
     <message>
-        <location filename="../multilayerselect.py" line="230"/>
+        <location filename="../multilayerselect.py" line="231"/>
         <source>Deselect Features from All Layers</source>
         <translation>Deselect Features from All Layers</translation>
     </message>
@@ -77,47 +77,47 @@
 <context>
     <name>MultiLayerSelectionExpressionBuilder</name>
     <message>
-        <location filename="../multiselectionexpressionbuilder.py" line="69"/>
+        <location filename="../multiselectionexpressionbuilder.py" line="68"/>
         <source>Close</source>
         <translation>Close</translation>
     </message>
     <message>
-        <location filename="../multiselectionexpressionbuilder.py" line="72"/>
+        <location filename="../multiselectionexpressionbuilder.py" line="71"/>
         <source>Zoom</source>
         <translation>Zoom</translation>
     </message>
     <message>
-        <location filename="../multiselectionexpressionbuilder.py" line="77"/>
+        <location filename="../multiselectionexpressionbuilder.py" line="76"/>
         <source>Select Features</source>
         <translation>Select Features</translation>
     </message>
     <message>
-        <location filename="../multiselectionexpressionbuilder.py" line="81"/>
+        <location filename="../multiselectionexpressionbuilder.py" line="80"/>
         <source>Add to selection</source>
         <translation>Add to selection</translation>
     </message>
     <message>
-        <location filename="../multiselectionexpressionbuilder.py" line="85"/>
+        <location filename="../multiselectionexpressionbuilder.py" line="84"/>
         <source>Remove from selection</source>
         <translation>Remove from selection</translation>
     </message>
     <message>
-        <location filename="../multiselectionexpressionbuilder.py" line="89"/>
+        <location filename="../multiselectionexpressionbuilder.py" line="88"/>
         <source>Filter selection</source>
         <translation>Filter selection</translation>
     </message>
     <message>
-        <location filename="../multiselectionexpressionbuilder.py" line="199"/>
+        <location filename="../multiselectionexpressionbuilder.py" line="198"/>
         <source>No matching features found</source>
         <translation>No matching features found</translation>
     </message>
     <message>
-        <location filename="../multiselectionexpressionbuilder.py" line="56"/>
+        <location filename="../multiselectionexpressionbuilder.py" line="55"/>
         <source>Help</source>
         <translation>Help</translation>
     </message>
     <message numerus="yes">
-        <location filename="../multiselectionexpressionbuilder.py" line="187"/>
+        <location filename="../multiselectionexpressionbuilder.py" line="186"/>
         <source>Zoomed to {0} matching feature(s)</source>
         <comment>matching feature count</comment>
         <translation>
@@ -126,7 +126,7 @@
         </translation>
     </message>
     <message>
-        <location filename="../multiselectionexpressionbuilder.py" line="64"/>
+        <location filename="../multiselectionexpressionbuilder.py" line="63"/>
         <source>Only use active layer</source>
         <translation>Only use active layer</translation>
     </message>
@@ -134,12 +134,12 @@
 <context>
     <name>MultiSelectTool</name>
     <message>
-        <location filename="../utils.py" line="50"/>
+        <location filename="../utils.py" line="49"/>
         <source>No features selected</source>
         <translation>No features selected</translation>
     </message>
     <message numerus="yes">
-        <location filename="../utils.py" line="64"/>
+        <location filename="../utils.py" line="63"/>
         <source>{0} features selected on layers {1}</source>
         <translation>
             <numerusform>{0} feature selected on layers {1}</numerusform>
@@ -147,7 +147,7 @@
         </translation>
     </message>
     <message numerus="yes">
-        <location filename="../utils.py" line="58"/>
+        <location filename="../utils.py" line="57"/>
         <source>{0} features selected on layer {1}</source>
         <translation>
             <numerusform>{0} feature selected on layer {1}</numerusform>
@@ -158,7 +158,7 @@
 <context>
     <name>MultiSelectionRadiusTool</name>
     <message>
-        <location filename="../maptools.py" line="454"/>
+        <location filename="../maptools.py" line="427"/>
         <source>Selection radius</source>
         <translation>Selection radius</translation>
     </message>
@@ -166,79 +166,84 @@
 <context>
     <name>SettingsDialog</name>
     <message>
-        <location filename="../settingsdialog.py" line="120"/>
+        <location filename="../settingsdialog.ui" line="14"/>
         <source>MultiLayer Select Settings</source>
         <translation>MultiLayer Select Settings</translation>
     </message>
     <message>
-        <location filename="../settingsdialog.py" line="123"/>
+        <location filename="../settingsdialog.ui" line="22"/>
         <source>Selection color</source>
         <translation>Selection color</translation>
     </message>
     <message>
-        <location filename="../settingsdialog.py" line="124"/>
+        <location filename="../settingsdialog.ui" line="61"/>
         <source>Set active layer from selected feature</source>
         <translation>Set active layer from selected feature</translation>
     </message>
     <message>
-        <location filename="../settingsdialog.py" line="127"/>
+        <location filename="../settingsdialog.ui" line="68"/>
         <source>Replace default selection actions (BETA)</source>
         <translation>Replace default selection actions (BETA)</translation>
     </message>
     <message>
-        <location filename="../settingsdialog.py" line="130"/>
+        <location filename="../settingsdialog.ui" line="75"/>
         <source>Display settings action on toolbar</source>
         <translation>Display settings action on toolbar</translation>
     </message>
     <message>
-        <location filename="../settingsdialog.py" line="169"/>
+        <location filename="../settingsdialog.ui" line="206"/>
         <source>Ok</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../settingsdialog.py" line="133"/>
+        <location filename="../settingsdialog.ui" line="137"/>
         <source>Allow to define layers that will not be considered by the multiselection tools (i.e. vector basemaps)</source>
         <translation>Allow to define layers that will not be considered by the multiselection tools (i.e. vector basemaps)</translation>
     </message>
     <message>
-        <location filename="../settingsdialog.py" line="139"/>
+        <location filename="../settingsdialog.ui" line="101"/>
         <source>Included Layers</source>
         <translation>Included Layers</translation>
     </message>
     <message>
-        <location filename="../settingsdialog.py" line="140"/>
+        <location filename="../settingsdialog.ui" line="117"/>
         <source>If checked, the active layer will be handled by the selection tools even if it is unchecked in the list below</source>
         <translation>If checked, the active layer will be handled by the selection tools even if it is unchecked in the list below</translation>
     </message>
     <message>
-        <location filename="../settingsdialog.py" line="155"/>
+        <location filename="../settingsdialog.ui" line="120"/>
         <source>Always include active layer</source>
         <translation>Always include active layer</translation>
     </message>
     <message>
-        <location filename="../settingsdialog.py" line="158"/>
-        <source>If checked, the scale based visibility will be disabled by the selection tools</source>
-        <translation>If checked, the scale based visibility will be disabled by the selection tools</translation>
-    </message>
-    <message>
-        <location filename="../settingsdialog.py" line="167"/>
+        <location filename="../settingsdialog.ui" line="146"/>
         <source>Include All</source>
         <translation>Include All</translation>
     </message>
     <message>
-        <location filename="../settingsdialog.py" line="168"/>
+        <location filename="../settingsdialog.ui" line="153"/>
         <source>Exclude All</source>
         <translation>Exclude All</translation>
     </message>
     <message>
-        <location filename="../settingsdialog.py" line="146"/>
-        <source>Exclude hidden layers</source>
-        <translation>Exclude hidden layers</translation>
+        <location filename="../settingsdialog.ui" line="107"/>
+        <source>If checked, ignore invisible layers (unchecked in layer tree)</source>
+        <translation>If checked, ignore invisible layers (unchecked in layer tree)</translation>
     </message>
     <message>
-        <location filename="../settingsdialog.py" line="164"/>
-        <source>Include hidden entities according to scale</source>
-        <translation>Include hidden entities according to scale</translation>
+        <location filename="../settingsdialog.ui" line="127"/>
+        <source>If checked, scale dependent visibility will be disabled while the selection occurs</source>
+        <translation>If checked, scale dependent visibility will be disabled while the selection occurs</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="130"/>
+        <source>Ignore scale-dependent visibility</source>
+        <translation>Ignore scale-dependent visibility</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="110"/>
+        <source>Exclude hidden layers</source>
+        <translation>Exclude hidden layers</translation>
     </message>
 </context>
 </TS>
